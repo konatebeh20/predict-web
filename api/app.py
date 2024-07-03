217937cbacd98856f9314e6de61cf555d697a61b
tornixtech <francis.amani@tornixtetch.com>
2024-07-03 17:58:17 +0000

    backend by konate beh

--- api/app.py
+++ api/app.py
@@ -1,29 +1,89 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+# from flask_cors import CORS
+# from flask_jwt_extended import JWTManager
+# from routes.cancer_prediction import cancer_prediction_bp  # Importez votre blueprint ici
+
+# # Création de l'application Flask
+# app = Flask(__name__)
+
+# # Configuration de la base de données SQLAlchemy
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Remplacez par l'URI de votre base de données
+# app.config['SECRET_KEY'] = 'your_secret_key'  # Remplacez par une clé secrète sécurisée
+# app.config['JWT_SECRET_KEY'] = 'jwt_secret_key'  # Remplacez par une clé JWT sécurisée
+
+# # Initialisation des extensions
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+# cors = CORS(app)
+# jwt = JWTManager(app)
+
+# # Enregistrement des blueprints
+# app.register_blueprint(cancer_prediction_bp, url_prefix='/api')
+
+# # Import des modèles SQLAlchemy
+# from models import PatientInfo, GenomicData, TranscriptomicData, ...  # Importez vos modèles ici
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from routes.cancer_prediction import cancer_prediction_bp  # Import blueprint
+from routes.cancer_prediction import cancer_prediction_bp  # Importez votre blueprint ici
 
-# Create Flask app
+# Création de l'application Flask
 app = Flask(__name__)
 
-# Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Replace with your actual database URI
-app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
-app.config['JWT_SECRET_KEY'] = 'jwt_secret_key'  # Replace with a secure JWT secret key
+# Configuration de la base de données SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Remplacez par l'URI de votre base de données
+app.config['SECRET_KEY'] = 'your_secret_key'  # Remplacez par une clé secrète sécurisée
+app.config['JWT_SECRET_KEY'] = 'jwt_secret_key'  # Remplacez par une clé JWT sécurisée
 
-# Initialize extensions
+# Initialisation des extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 cors = CORS(app)
 jwt = JWTManager(app)
 
-# Register blueprints
+# Enregistrement des blueprints
 app.register_blueprint(cancer_prediction_bp, url_prefix='/api')
 
-# Import models
-from models import PatientInfo, GenomicData, TranscriptomicData, ...  # Import your models
+# Import des modèles SQLAlchemy
+from models import PatientInfo, GenomicData, TranscriptomicData, ...  # Importez vos modèles ici
 
 if __name__ == '__main__':
     app.run(debug=True)
